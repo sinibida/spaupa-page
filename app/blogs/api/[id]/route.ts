0bd0b710148079d6db699dc89353c5c9e040fdfb
SPAUPA <40449398+sinibida@users.noreply.github.com>
2023-06-15 01:39:27 +0900

    lastfixpleasepleasasea

--- app/blogs/api/[id]/route.ts
+++ app/blogs/api/[id]/route.ts
@@ -5,6 +5,7 @@ import { google } from "googleapis";
 import { GoogleAuth } from "google-auth-library";
 import { Readable } from "stream";
 import { getDrive, getFileContent } from "../drive";
+import { BlogPost } from "../util";
 
 interface Params {
     params: {
@@ -21,14 +22,18 @@ export async function GET(request: Request, {params}: Params) {
         q: `'${process.env.DRIVE_FOLDER_ID}' in parents and name = '${id}.mdx'`
     })
     if (!(searchResult.data.files) || searchResult.data.files.length == 0) {
-        return NextResponse.json("Invalid Id", {status: 400});
+        return NextResponse.json<BlogPost>({
+            path: "",
+            source: "",
+            error: "Invalid Id"
+        }, {status: 400});
     }
     const result = searchResult.data.files[0];
 
     const source = await getFileContent(drive, result.id!);
 
     //return NextResponse.json(GetMDX(id));
-    return NextResponse.json({
+    return NextResponse.json<BlogPost>({
         source,
         path: id,
     });
